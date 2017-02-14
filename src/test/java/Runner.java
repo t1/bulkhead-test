249300e60959cc149e81422fd43e229e380c262d
Rüdiger zu Dohna <snackbox@sinntr.eu>
2017-02-14 17:33:09 +0100

    optimized logging

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.URI;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.*;
 
 import static java.util.concurrent.TimeUnit.*;
@@ -9,29 +9,36 @@ public class Runner {
     private static final ExecutorService EXECUTORS = Executors.newCachedThreadPool();
 
     public static void main(String[] args) throws Exception {
+        List<Future<String>> futures = new ArrayList<>();
         long t = System.currentTimeMillis();
         for (int i = 0; i < 25; i++) {
             int finalI = i;
-            EXECUTORS.submit(() -> pingPong(finalI));
+            futures.add(EXECUTORS.submit(() -> pingPong(finalI)));
         }
         EXECUTORS.shutdown();
+        System.out.println("results: ");
+        for (Future<String> future : futures)
+            System.out.println("-> " + future.get(10, SECONDS));
         EXECUTORS.awaitTermination(1, MINUTES);
         System.out.println("total time: " + (System.currentTimeMillis() - t));
     }
 
-    private static void pingPong(int ping) {
-        System.out.println("ping " + ping);
+    private static String pingPong(int ping) {
+        // System.out.println("ping " + ping);
         URI base = URI.create("http://localhost:8080/bulkhead/" + ping + "?wait=1000");
-        String pong = read(base);
-        System.out.println("pong " + pong);
-        assert ("pong:" + ping).equals(pong);
+        try {
+            String pong = read(base);
+            // System.out.println("pong " + pong);
+            assert ("pong:" + ping).equals(pong);
+            return ping + ":ok";
+        } catch (IOException e) {
+            return ping + ":failed: " + e.getMessage();
+        }
     }
 
-    private static String read(URI uri) {
+    private static String read(URI uri) throws IOException {
         try (InputStream stream = uri.toURL().openConnection().getInputStream()) {
             return new Scanner(stream).useDelimiter("\\Z").next();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
